600e8425a8ed1c38216283a114653915f16f7491
DaviiJones <davonbridgett@pursuit.org>
2023-08-20 11:12:14 -0500

    me and jeff

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import React from "react";
 import postData from "./data/posts.json";
 
 function App() {
+  function createArt() {
+    return fetch("./posts.json");
+  }
+
   console.log(postData);
+
   return (
-    <main>
-      <button className="btn">hi</button>
-    </main>
+    <div className="mb-3">
+      <div className="header">
+        <h1>Travel Blog</h1>
+      </div>
+      <ul>
+        <li>
+          <a href="">About</a>
+
+
+        </li>
+        <li>
+          <a href="">Popular Post</a>
+        </li>
+      </ul>
+      <div className="">
+        <h2>Travel is the only thing you buy that makes you richer...</h2>
+      </div>
+      <main>
+        <button className="btn">hi</button>
+      </main>
+    </div>
   );
 }
 
